ed32264e72dc9fca73dcc69f42ae8c08dc1cd2a8
Yuki (aka Ruben Gomez) <rugoli@gmail.com>
2019-07-05 13:55:49 +0200

    Updates

---
+++ precache-manifest.07314a05170ff0b990b2cb9ade2c5b18.js
@@ -0,0 +1,148 @@
+self.__precacheManifest = [
+  {
+    "revision": "0551e343c2c82573a311",
+    "url": "app.css"
+  },
+  {
+    "revision": "0551e343c2c82573a311",
+    "url": "app.js"
+  },
+  {
+    "revision": "ebbd808b442ebd8bf14d",
+    "url": "hljs-langs/bash.js"
+  },
+  {
+    "revision": "6f40e02088b60bfbb7ab",
+    "url": "hljs-langs/pgsql.js"
+  },
+  {
+    "revision": "0a129c3cdc63f533898b",
+    "url": "hljs-langs/sql.js"
+  },
+  {
+    "revision": "6e29ff6d4e03bd5ba8b3d5556174789e",
+    "url": "b97c530867a61ea60506b10a416762c4.svg"
+  },
+  {
+    "revision": "9d9be852aec6202f99a33cf9cc292d9c",
+    "url": "95c7b5a26ab51d96ee39f36c66005d67.svg"
+  },
+  {
+    "revision": "8a63aaf795c65b9a001dfc6bf7387293",
+    "url": "66cec3167484d42c1ccc513c6259a4f8.svg"
+  },
+  {
+    "revision": "5d039d9f3408836ef24841905f04b69b",
+    "url": "1ba934df20c60bfbda92ae00e4ac1b37.svg"
+  },
+  {
+    "revision": "43b843880f019c3bdf2a047b201961a7",
+    "url": "187a99d3fe4e62c75737c5f4a93e00a6.svg"
+  },
+  {
+    "revision": "e688707ed86fca73e90fd1e2d3dc4645",
+    "url": "a0296bb86117cab30ef27fd515a87f79.svg"
+  },
+  {
+    "url": "3e1af3ef546b9e6ecef9f3ba197bf7d2.ttf"
+  },
+  {
+    "url": "fc84e998bc29b297ea20321e4c90b6ed.ttf"
+  },
+  {
+    "url": "89e2666c24d37055bcb60e9d2d9f7e35.ttf"
+  },
+  {
+    "url": "ec4c9962ba54eb91787aa93d361c10a8.ttf"
+  },
+  {
+    "url": "ee7b96fa85d8fdb8c126409326ac2d2b.ttf"
+  },
+  {
+    "url": "42bbe4eefcde1297b11dc4b6491e9746.ttf"
+  },
+  {
+    "url": "d1efcd4d126837fe0dcf9b6cf3a00d64.ttf"
+  },
+  {
+    "url": "0fc25386220a58203994ce45fb4ae570.ttf"
+  },
+  {
+    "url": "50705c5ed1205b63efdbfee941a6b655.ttf"
+  },
+  {
+    "url": "1eb7a893589ddce89d81cdb22a356660.ttf"
+  },
+  {
+    "revision": "8ec6268e47b6b2ca18a0b054471be6b4",
+    "url": "3rd.party.LICENSES"
+  },
+  {
+    "revision": "0c01f56a9d2b0fc705d8ffd7a01b4e4b",
+    "url": "media/backup.svg"
+  },
+  {
+    "revision": "e02a12a2c6c6b130a53a062b2eaa5e07",
+    "url": "media/cluster.svg"
+  },
+  {
+    "revision": "71ecf6046a929375cf822830c0c93609",
+    "url": "media/docker_logo.svg"
+  },
+  {
+    "revision": "d348e471899dc29c79c623ebaa62e0a3",
+    "url": "media/logo-mysql.svg"
+  },
+  {
+    "revision": "b8efd355c94aaee08a6b079e7f56be6b",
+    "url": "media/replication.svg"
+  },
+  {
+    "revision": "5ff8abb1a57b5e3f10660d7d3a6b44b5",
+    "url": "media/backup-incremental.svg"
+  },
+  {
+    "revision": "ea7541bd4220a68b179ff3e434e478fe",
+    "url": "media/irontec-logo-white.png"
+  },
+  {
+    "revision": "266eae7232fc7b0692dedf3f3a4dc020",
+    "url": "media/backup-incremental.png"
+  },
+  {
+    "revision": "aafded1341e00fbad9b0b4f157034923",
+    "url": "media/proxysql-cluster.svg"
+  },
+  {
+    "revision": "d3859c27b3dc0347abe862e39bcedf29",
+    "url": "media/xtrabackup.png"
+  },
+  {
+    "revision": "854fe2069f8ef71eef54b336f03c02e5",
+    "url": "media/cluster_control_screenshot.png"
+  },
+  {
+    "revision": "ba7070f24d3968c014875a881438e4db",
+    "url": "media/proxysql-cluster-2.svg"
+  },
+  {
+    "revision": "9dba5b179daf1397bd43bd8f61e59e7a",
+    "url": "media/xtradbcluster.png"
+  },
+  {
+    "revision": "a73a7a2f51741472894569421fd32e39",
+    "url": "media/proxysql.png"
+  },
+  {
+    "revision": "5c4865e176a076526f5a91f6e61c0105",
+    "url": "media/cluster_control.svg"
+  },
+  {
+    "revision": "702ccb575b9cfd5899d504a2f76e7c5d",
+    "url": "index.html"
+  },
+  {
+    "revision": "27ddf47c7a7de1b45858d6d59a4017f5",
+    "url": "notes.html"
+  }
+];
