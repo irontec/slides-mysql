a6fbdd273768c1974b235e516005144ecce4a4e6
Javier Infante <jabi@irontec.com>
2019-10-07 17:04:00 +0200

    Updates

---
+++ precache-manifest.f4d5c03a6b4cfb8f97279c4b6048c090.js
@@ -0,0 +1,155 @@
+self.__precacheManifest = [
+  {
+    "revision": "6152ba0368892da2664c",
+    "url": "app.css"
+  },
+  {
+    "revision": "6152ba0368892da2664c",
+    "url": "app.js"
+  },
+  {
+    "revision": "f7f4e93b3c7ccd22e3a9",
+    "url": "hljs-langs/bash.js"
+  },
+  {
+    "revision": "698267d84584f7917900",
+    "url": "hljs-langs/pgsql.js"
+  },
+  {
+    "revision": "6c12944c388449b4fb4f",
+    "url": "hljs-langs/sql.js"
+  },
+  {
+    "url": "ec405da9534f47c8512d799f50a5f913.jpg"
+  },
+  {
+    "url": "89e2666c24d37055bcb60e9d2d9f7e35.ttf"
+  },
+  {
+    "url": "42bbe4eefcde1297b11dc4b6491e9746.ttf"
+  },
+  {
+    "url": "d1efcd4d126837fe0dcf9b6cf3a00d64.ttf"
+  },
+  {
+    "url": "0fc25386220a58203994ce45fb4ae570.ttf"
+  },
+  {
+    "url": "ec4c9962ba54eb91787aa93d361c10a8.ttf"
+  },
+  {
+    "url": "50705c5ed1205b63efdbfee941a6b655.ttf"
+  },
+  {
+    "url": "ee7b96fa85d8fdb8c126409326ac2d2b.ttf"
+  },
+  {
+    "url": "1eb7a893589ddce89d81cdb22a356660.ttf"
+  },
+  {
+    "url": "fc84e998bc29b297ea20321e4c90b6ed.ttf"
+  },
+  {
+    "url": "3e1af3ef546b9e6ecef9f3ba197bf7d2.ttf"
+  },
+  {
+    "revision": "bf45ef848f036d3234652ac6795328e7",
+    "url": "b97c530867a61ea60506b10a416762c4.svg"
+  },
+  {
+    "revision": "172133917c3c23a27512b50927e278b9",
+    "url": "66cec3167484d42c1ccc513c6259a4f8.svg"
+  },
+  {
+    "revision": "5d039d9f3408836ef24841905f04b69b",
+    "url": "1ba934df20c60bfbda92ae00e4ac1b37.svg"
+  },
+  {
+    "revision": "43b843880f019c3bdf2a047b201961a7",
+    "url": "187a99d3fe4e62c75737c5f4a93e00a6.svg"
+  },
+  {
+    "revision": "9801ff92c1d4e5778d71878ab20bb7a8",
+    "url": "95c7b5a26ab51d96ee39f36c66005d67.svg"
+  },
+  {
+    "revision": "e688707ed86fca73e90fd1e2d3dc4645",
+    "url": "a0296bb86117cab30ef27fd515a87f79.svg"
+  },
+  {
+    "revision": "f4ad85850132f22b0e42449b46410293",
+    "url": "3rd.party.LICENSES"
+  },
+  {
+    "revision": "ec405da9534f47c8512d799f50a5f913",
+    "url": "media/fondo_presentacion.jpg"
+  },
+  {
+    "revision": "266eae7232fc7b0692dedf3f3a4dc020",
+    "url": "media/backup-incremental.png"
+  },
+  {
+    "revision": "0c01f56a9d2b0fc705d8ffd7a01b4e4b",
+    "url": "media/backup.svg"
+  },
+  {
+    "revision": "5ff8abb1a57b5e3f10660d7d3a6b44b5",
+    "url": "media/backup-incremental.svg"
+  },
+  {
+    "revision": "854fe2069f8ef71eef54b336f03c02e5",
+    "url": "media/cluster_control_screenshot.png"
+  },
+  {
+    "revision": "d86e67eeb77d439553cb04158862589f",
+    "url": "media/docker_logo.svg"
+  },
+  {
+    "revision": "e02a12a2c6c6b130a53a062b2eaa5e07",
+    "url": "media/cluster.svg"
+  },
+  {
+    "revision": "15c914f55d2dd687dfb9ee01356a071d",
+    "url": "media/cluster_control.svg"
+  },
+  {
+    "revision": "ea7541bd4220a68b179ff3e434e478fe",
+    "url": "media/irontec-logo-white.png"
+  },
+  {
+    "revision": "9e6f49b75f13d40102f93cc1bc0c1334",
+    "url": "media/logo-mysql.svg"
+  },
+  {
+    "revision": "1c4b46c07768500b0bc79ce105d613f1",
+    "url": "media/proxysql-cluster-2.svg"
+  },
+  {
+    "revision": "aafded1341e00fbad9b0b4f157034923",
+    "url": "media/proxysql-cluster.svg"
+  },
+  {
+    "revision": "d3859c27b3dc0347abe862e39bcedf29",
+    "url": "media/xtrabackup.png"
+  },
+  {
+    "revision": "a73a7a2f51741472894569421fd32e39",
+    "url": "media/proxysql.png"
+  },
+  {
+    "revision": "b8efd355c94aaee08a6b079e7f56be6b",
+    "url": "media/replication.svg"
+  },
+  {
+    "revision": "9dba5b179daf1397bd43bd8f61e59e7a",
+    "url": "media/xtradbcluster.png"
+  },
+  {
+    "revision": "c28e8a90511155dcd3e8373366bc4817",
+    "url": "index.html"
+  },
+  {
+    "revision": "034352a117c0b53e468095354adf78ce",
+    "url": "notes.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.dd76888adb23845ff657eb7da3a5e38b.js"
+  "precache-manifest.f4d5c03a6b4cfb8f97279c4b6048c090.js"
 );
 
 workbox.skipWaiting();
